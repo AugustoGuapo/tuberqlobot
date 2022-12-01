dd2c7db106b980b17721da75221045a48b492e54
Augusto Diaz <adiazpol17@correo.unicordoba.edu.co>
2022-11-30 23:54:18 -0500

    actualizacion navideña2

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ require("dotenv").config();
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.MessageContent]
 });
 */
-
+var membersAlreadyPlayed = []
 const Discord = require('discord.js');
 const client = new Discord.Client({
     intents: [
@@ -64,6 +64,26 @@ client.on('interactionCreate', (int) =>{
         int.reply(config.ayuda);
     }
 })
+client.on('voiceStateUpdate', (oldState, newState) => {
+  const memberId = oldState.member.id
+  const queue = client.distube.getQueue(oldState.member.guild.id)
+  var isQueued = false
+  if(queue != null){
+    for (let index = 0; index < queue.songs.length; index++) {
+      const element = queue.songs[index];
+      if(element.name === 'Mariah Carey - All I Want For Christmas Is You (Lyrics)') isQueued = true;
+  }
+  }
+
+  if(oldState.channel === null && !membersAlreadyPlayed.includes(memberId) && !isQueued) {
+    client.distube.play(oldState.member.voice.channel, 'All I want for christmas is you', {
+      member: oldState.member,
+      textChannel: oldState.guild.channels.cache.find(channel => channel.name.toLowerCase() === 'general')
+
+    })
+    membersAlreadyPlayed.push(memberId)
+  }
+});
 
 client.on('messageCreate', async message => {
     if(message.author.bot || !message.guild) return;
@@ -72,20 +92,22 @@ client.on('messageCreate', async message => {
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
 
     if(!message.content.toLocaleLowerCase().startsWith(prefix)) return;
-    if(message.guild.id == "948433193265156126") return message.channel.send('ayura toi secuestrao debuelban las waifus a mi creador D:');
+    //if(message.guild.id == "948433193265156126") return message.channel.send('ayura toi secuestrao debuelban las waifus a mi creador D:'); linea para bloquear el servidor de mey
 
     const comm = args.shift().toLowerCase();
-    if(!message.member.voice.channel) return message.channel.send('entra en un canal primero tonto')
-    if(comm === 'jugar') {
-        if (!message.member.voice.channel) {
-          return message.channel.send('entra en un canal primero tonto');
-        }
-        if(args.join(" ") === "") return message.channel.send('no soi tonto eso ta basio');
-        client.distube.play(message.member.voice.channel, args.join(" "), {
-            member: message.member,
-            textChannel: message.channel,
-            message
-        })
+    const isInChannel = message.member.voice.channel
+    const musicCommands = ['jugar', 'pausa', 'detener', 'skip', 'volumen', 'adio']
+    
+    //COMANDOS DE MÚSICA
+    if(musicCommands.includes(comm)){ //Verifica que el comando escrito sea de música
+      if(isInChannel) { //Verifica que quien pidió el comando esté dentro de un canal de voz
+        if(comm === 'jugar') {
+          if(args.join(" ") === "") return message.channel.send('no soi tonto eso ta basio');
+          client.distube.play(message.member.voice.channel, args.join(" "), {
+              member: message.member,
+              textChannel: message.channel,
+              message
+          })
     }
     else if (comm === 'pausa') {
         const queue = client.distube.getQueue(message)
@@ -131,8 +153,11 @@ client.on('messageCreate', async message => {
         client.distube.voices.leave(message)
       }).catch(error => console.log(error));
     }
+      }
+      else return message.channel.send('entra en un canal primero tonto') //Envía un mensaje si se usó un comando de música pero el usuario no está en ningun canal
+    }
 
-    else if(comm == 'teamo') {
+    if(comm == 'teamo') {
       const randomNumber = Math.floor(Math.random() * 2)
       const outputMessage = randomNumber === 1 ? 'yo ati <3': 'grasias'
       return message.channel.send(outputMessage)
@@ -162,6 +187,44 @@ client.on('messageCreate', async message => {
       return message.channel.send('mmmmmmm mi recomendasion como profecional de las peliculas es k beas: ' + peliculas[randomNumber])
     }
 */
+
+    /*
+    COMANDOS PARA NAVIDAD
+    */
+
+    else if(comm === 'muerdago') {
+      const memberId = message.member.id
+      var outputMessage = memberId == '250464961418100737' || memberId == '514162096733159424' || memberId == '751711526989332511' || memberId == '754123879244365995' || memberId == '419574059450499072' ? 'muakata😘' : 'fuchila no t bso🤢'
+      return message.channel.send(outputMessage)
+    }
+
+    else if(comm === 'cuento') {
+      const historias = ['breach nabideño\nabia una bes un pendejo k consigio a brich i lo gugo tam mal k no lo bolbio a uzar, y azi tdxs pasaron una felis nabida',
+      'bloops santa claus calva\nera se una bes una santa claus calva k le rega lo a un pato un cor tauñas fin']
+      const randomNumber = Math.floor(Math.random() * historias.length)
+      return message.channel.send('ai les ba una d mis istorias famosas\n' + historias[randomNumber])
+    }
+
+    else if (comm === 'gaitas') {
+      const gaitas = ["Pa' que Luis - Gaiteros de pillopo",
+      "La voy a tocar a pie - VHG", "Mi ranchito - Gran Coquivacoa",
+      "Amparito - Maracaibo 15", "Gaita onomatopéyica - Gran Coquivacoa",
+      "Son mis deseos - Cardenales del éxito", "Sin rencor - Gran Coquivacoa",
+      "Viejo año - Maracaibo 15", "Venga un abrazo - Maracaibo 15", "Afina ese cuatro - Maracaibo 15",
+      "El negrito fullero - Cardenales del éxito"]
+      const randomNumber = Math.floor(Math.random() * gaitas.length)
+      return message.channel.send("mmmmmmm mi rceomendasion como profecional de las gaitas es k eskuches : " + gaitas[randomNumber])
+    }
+
+    else if (comm === 'felis') {
+      return message.channel.send('FELIS NABIDA ' + message.author.username.toUpperCase())
+    }
+
+    else if (comm === 'regalo') {
+      return message.member.send('toma tu regalo')
+    }
+
+
 })
 
 const status = queue =>
